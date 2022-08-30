261116a20310b4b6673123949e6b004798b8445e
santiagoduranaguirre <santiduran1995@gmail.com>
2022-08-29 19:26:21 -0500

    new commit

---
+++ src/components/Cabecera.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+// El componente Cabera no tiene componentes hijos.
+// ESTADO: Cabecera no tiene estado.
+// MÉTODOS: Cabecera no tiene métodos.
+// PROPS: Cabecera recibe de su padre la cantidad que va a mostrar dentro del span correpondiente
+// Maqueta de Cabecera:
+//    h1
+//    p > span     (el span mostrará la cantidad recibida por props)
+
+export default function Cabecera({carrito}) {
+  
+  return (
+    <header>
+      <h1>Carrito de compras</h1>
+      <p> <span>{carrito}</span></p>
+        {/* maquetar Cabecera aquí */}
+    </header>
+  )
+}
