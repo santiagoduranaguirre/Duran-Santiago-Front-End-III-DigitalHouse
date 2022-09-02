1545033d734f7c63960d53f0facb26b5db628400
santiagoduranaguirre <santiduran1995@gmail.com>
2022-09-01 20:21:21 -0500

    Quitando comentarios

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
-// El componente App es el padre de:
-// - Cabecera
-// - Listado
-// ESTADO: App debe manejar en su estado un número para contabilizar el total de elementos comprados.
-// MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
-// PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
 import Cabecera from "./components/Cabecera";
 import Listado from "./components/Listado";
-import data from "./components/data.json"
 import { useState } from "react";
 
-
-
 function App() {
-
-  const [carrito, setCarrito] = useState(0)
+  const [carrito, setCarrito] = useState(0);
 
   const comprar = () => {
-    setCarrito(carrito + 1)
-  }
+    setCarrito(carrito + 1);
+  };
 
   return (
     <div className="App">
       <Cabecera carrito={carrito} />
       <Listado comprar={comprar} />
-    </div> 
+    </div>
   );
 }
 
--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,24 +1,20 @@
-import React from 'react'
-import { useState } from 'react'
-import Item from './Item'
-import lista from './data.json'
+import React from "react";
+import { useState } from "react";
+import Item from "./Item";
+import lista from "./data.json";
 
-// Debemos importar el listado de objetos del archivo JSON para usarlos como array a la hora de crear cada Item.
-// El componente Listado es el padre de:
-// - varios Item
-// ESTADO: Listado no necesita manejar un estado.
-// MÉTODOS: Listado no requiere de métodos.
-// PROPS: Listado recibe el método para aumentar el estado de App y se lo pasa a cada uno de sus hijos.
-
-export default function Listado({comprar}) {
-
-
-
+export default function Listado({ comprar }) {
   return (
-    <div className='container'>
-      {lista.map(producto => 
-        <Item comprar={comprar} key={producto.id} nombre={producto.producto.nombre} descripcion={producto.producto.descripcion} stock={producto.stock} />
-        )} 
+    <div className="container">
+      {lista.map((producto) => (
+        <Item
+          comprar={comprar}
+          key={producto.id}
+          nombre={producto.producto.nombre}
+          descripcion={producto.producto.descripcion}
+          stock={producto.stock}
+        />
+      ))}
     </div>
-  )
+  );
 }
